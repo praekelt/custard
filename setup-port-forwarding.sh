3de0fb0a9badcff69ddcbe9810eccb151e548c8c
Simon de Haan <simon@praekeltfoundation.org>
2015-06-25 17:06:04 +0200

    changing forwarding script for banele

--- setup-port-forwarding.sh
+++ setup-port-forwarding.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 ssh -L localhost:2005:prd-gsm:2005 \
     -L localhost:2011:prd-gsm:2011 \
-    sdehaan@prd-txtalert-gateway.44st.prk-host.net \
+    "$@"@prd-txtalert-gateway.44st.prk-host.net \
     -t 
