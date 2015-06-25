b58ac71a3cba303c0a12d77e16b364444084331e
mtsban004@myuct.ac.za <Skill-Zozo>
2015-06-25 14:42:47 +0200

    port forwarding with through port 2002

--- setup-port-forwarding.sh
+++ setup-port-forwarding.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-ssh -L localhost:2001:prd-gsm:2001 banele@prd-txtalert-gateway.44st.prk-host.net -t top
+ssh -L localhost:2002:prd-gsm:2002 banele@prd-txtalert-gateway.44st.prk-host.net -t top
