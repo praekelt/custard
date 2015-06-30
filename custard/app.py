e5e41cbcbf2d24578662c817431341a0fba620cc
Simon de Haan <simon@praekeltfoundation.org>
2015-06-30 15:11:08 +0200

    wait for response before sending next lines

--- custard/app.py
+++ custard/app.py
@@ -1,55 +1,58 @@
 from twisted.python import log
-from twisted.internet.defer import inlineCallbacks
-from twisted.internet import reactor
+from twisted.internet.defer import inlineCallbacks, succeed
 import parsley
 
 
 class CommandLineApp(object):
     ussd_resp = None
+
     def __init__(self, protocol, options):
         self.protocol = protocol
         self.protocol.verbose = options['verbose']
         self.load_script(options)
-        
+
     def load_script(self, options):
         with open(options['script'], 'r') as fp:
             self.script = fp.read()
 
     def on_modem_ready(self, configure_response):
         log.msg('Modem is ready!', configure_response)
-        self.run_script_test()
+        return self.run_script_test()
 
     def convert_to_ussd(self, code):
-        message = "at+cusd=1,%s,15" % (code,)
+        message = "AT+CUSD=1,%s,15" % (code,)
         return message
 
+    @inlineCallbacks
     def run_script_test(self):
         for line in self.script.split('\n'):
             if line:
-                self.decryptLine(line)
+                response = yield self.decryptLine(line)
+                print 'got response!', response
 
-    def ColorIt(self, output, color):  
+    def ColorIt(self, output, color):
             attr = []
             if color == "red":
-                attr.append('31') 
+                attr.append('31')
             elif color == "green":
                 attr.append('32')
             attr.append('1')
             return '\x1b[%sm%s\x1b[0m' % (';'.join(attr), output)
-    
+
     def handle_response(self, message):
-        print "Popped"
+        print "Popped", '\n'.join(message['response'])
         cleanResponse = message.lstrip("+CUSD: ")
         cleanResponse = cleanResponse.replace(self.delimiter,"\n")
         ussd_response = cleanResponse[3:-5]
         self.ussd_resp = ussd_response
+        return succeed(ussd_response)
 
-    @inlineCallbacks
     def dial(self, code):
         code = self.convert_to_ussd(code)
-        self.response =  yield self.protocol.send_command(code, expect='+CUSD')
-        self.response.addCallback(self.handle_response)
+        d = self.protocol.send_command(code, expect='+CUSD')
+        d.addCallback(self.handle_response)
         print "response set"
+        return d
 
     def expect(self, code):
 #        if self.ussd_resp == None:
@@ -75,26 +78,26 @@ class CommandLineApp(object):
 
     def decryptLine(self, line):
         x = parsley.makeGrammar(
-            """ 
-            digit = anything:c ?(c in '0123456789*#') 
+            """
+            digit = anything:c ?(c in '0123456789*#')
             word = anything:c ?(c != ' ')
-            ws = ' '* 
+            ws = ' '*
             instruction = <word+>:pika ws <anything*>:dee-> expression(pika,dee)
-            """, 
+            """,
             {"expression":self.expression}
         )
 
-        x(line).instruction()
+        return x(line).instruction()
 
     def expression(self, instr, num):
         instr = instr.lower()
         if instr == "dial":
-            self.dial(num)
+            return self.dial(num)
         elif instr == "expect":
-            self.expect(num)
+            return self.expect(num)
         elif instr == "send":
-            self.send(num)
+            return self.send(num)
         elif instr == "end":
-            self.end()
+            return self.end()
         else:
             print "Unknown instruction: ", instr
